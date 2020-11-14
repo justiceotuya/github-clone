cf30b81cdab1551760813709a8c958d3e9b805ea
Justice Otuya <jotuya2@gmail.com>
2020-11-14 18:56:26 +0100

    finish mobile navigation

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+// toggle open and close state of header when hamburger is clicked
+let hamburgerButton = document.querySelector('.header__hamburger');
+let headerLinkSection = document.querySelector('.header__bottom');
+hamburgerButton.addEventListener('click', function() {
+    if(headerLinkSection.classList.contains('hidden')){
+        headerLinkSection.classList.remove('hidden')
+    }else {
+        headerLinkSection.classList.add('hidden')
+    }
+});
